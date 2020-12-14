7a5d6d4b00c3d4471d559da697f1ef3d3e650761
minj0i <h_hoh@naver.com>
2020-12-15 00:03:39 +0900

    Post기능 및 테스트

---
+++ src/main/java/org/example/web/domain/posts/PostsService.java
@@ -0,0 +1,18 @@
+package org.example.web.domain.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.web.web.dto.PostsSaveRequestDto;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
