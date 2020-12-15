34463f988c350f8d065258a3178c18d100787a79
minj0i <h_hoh@naver.com>
2020-12-15 23:26:23 +0900

    Update PostsApiController.java

--- src/main/java/org/example/web/web/PostsApiController.java
+++ src/main/java/org/example/web/web/PostsApiController.java
@@ -3,9 +3,7 @@ package org.example.web.web;
 import lombok.RequiredArgsConstructor;
 import org.example.web.domain.posts.PostsService;
 import org.example.web.web.dto.PostsSaveRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +14,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/vi/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable) {
+        return postsService.findById(id);
     }
 }
